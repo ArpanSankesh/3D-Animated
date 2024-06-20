70b63569c9277c639cbba3b39dbdaf515c5bb6cc
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-20 21:59:22 +0530

    Animation using Vanta

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 VANTA.WAVES({
-    el: "#main",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00
-  })
+  el: "#main",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+});
 
-  let main = document.querySelector("#main");
-  let cursor = document.querySelector(".cursor");
-  let container = document.querySelector(".container");
+VANTA.GLOBE({
+  el: "#main",
+  mouseControls: true,
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0xb8ff,
+  backgroundColor: 0x0,
+});
 
-let move = (elem) =>{
-    cursor.style.top = elem.clientY + "px" ;
-    cursor.style.left = elem.clientX + "px";
-}
+let main = document.querySelector("#main");
+let cursor = document.querySelector(".cursor");
+let container = document.querySelector(".container");
 
-let move2 = (elem) =>{
-    cursor.style.top = elem.clientY + "px";
-    cursor.style.left = elem.clientX + "px";
-    cursor.style.backgroundColor = '#1c7fb9';
-}
+let move = (elem) => {
+  cursor.style.top = elem.clientY + "px";
+  cursor.style.left = elem.clientX + "px";
+};
 
-  main.addEventListener('mousemove', move)
-  container.addEventListener('mousemove', move2)
-  container.addEventListener('mouseleave', function(){
-    cursor.style.backgroundColor = '#fff';
-  })
+let move2 = (elem) => {
+  cursor.style.top = elem.clientY + "px";
+  cursor.style.left = elem.clientX + "px";
+  cursor.style.backgroundColor = "#1c7fb9";
+};
+
+main.addEventListener("mousemove", move);
+container.addEventListener("mousemove", move2);
+container.addEventListener("mouseleave", function () {
+  cursor.style.backgroundColor = "#fff";
+});
